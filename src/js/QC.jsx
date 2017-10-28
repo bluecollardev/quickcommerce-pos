f937f5dcc3c38ab741eba6e50f41076a1b69910d
Lucas <lucaslopatka@gmail.com>
2017-10-27 21:32:25 -0600

    Updated to use mobx providers and injectors to facilitate context handling.

--- src/js/QC.jsx
+++ src/js/QC.jsx
@@ -34,11 +34,19 @@ import ContentArticle from './pages/ContentArticle'
 import LoginActions from 'quickcommerce-react/actions/LoginActions.jsx'
 import UserActions from 'quickcommerce-react/actions/UserActions.jsx'
 
+import AuthService from 'quickcommerce-react/services/AuthService.jsx'
 import CustomerService from 'quickcommerce-react/services/CustomerService.jsx'
+import CustomerAddressService from 'quickcommerce-react/services/CustomerAddressService.jsx'
+import CheckoutService from 'quickcommerce-react/services/CheckoutService.jsx'
 
 import LoginStore from 'quickcommerce-react/stores/LoginStore.jsx'
 import UserStore from 'quickcommerce-react/stores/UserStore.jsx'
-import AuthService from 'quickcommerce-react/services/AuthService.jsx'
+import CustomerStore from 'quickcommerce-react/stores/CustomerStore.jsx'
+import CustomerListStore from 'quickcommerce-react/stores/CustomerListStore.jsx'
+import ProductStore from 'quickcommerce-react/stores/ProductStore.jsx'
+import CheckoutStore from 'quickcommerce-react/stores/CheckoutStore.jsx'
+import SettingStore from 'quickcommerce-react/stores/SettingStore.jsx'
+import StarMicronicsStore from 'quickcommerce-react/stores/StarMicronicsStore.jsx'
 
 console.log('QC API endpoint: ' + QC_API)
 
@@ -142,8 +150,17 @@ class QC extends Component {
         return (
             <Provider 
                 authService = {AuthService}
+                customerService = {CustomerService}
+                customerAddressService = {CustomerAddressService}
+                customerStore = {CustomerStore}
+                customerListStore = {CustomerListStore}
+                checkoutService = {CheckoutService}
+                checkoutStore = {CheckoutStore}
+                productStore = {ProductStore}
                 loginStore = {LoginStore}
-                userStore = {UserStore}>
+                userStore = {UserStore}
+                settingStore = {SettingStore}
+                starMicronicsStore = {StarMicronicsStore}>
                 <AuthenticatedApp>
                     <div id='outer-container'>
                         <main id='page-wrap'>
